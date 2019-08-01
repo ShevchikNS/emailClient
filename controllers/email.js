927f63c4e92d855e2913a189bdd0c4a56e51cda2
shevchik <shevchikkola@gmail.com>
2019-08-01 11:17:51 +0300

    add email and password to database

--- controllers/email.js
+++ controllers/email.js
@@ -1,11 +1,19 @@
 const logger = require('../utils/logger');
+const service = require('../services/emaildb');
+
 module.exports = {
   showEmailAddPage: (req, res, next) => {
     res.render('addEmail');
   },
 
-  addEmail: (req, res, next) => {
-    console.log(req.body);
-    res.render('addEmail', { message: req.body.email });
+  addEmail: async (req, res, next) => {
+    const userId = req.user.id;
+    const data = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const email = await service.addEmail(userId, data);
+
+    res.send(email);
   },
 };
--- services/emaildb.js
+++ services/emaildb.js
@@ -1,15 +1,9 @@
-const express = require('express');
 const models = require('../models');
 
-// app = express();
-
-// app.get();
-// const app = express();
-// module.exports
-// app.use(async (req, res, next) => {
-//   const userId = req.user.id;
-//   const user = await models.User.FindByPk(id);
-// });
 module.exports = {
-  
-}
+  addEmail: async (userId, emailData) => {
+    const user = await models.User.findByPk(userId);
+    const email = await models.Email.create(emailData);
+    user.addEmail(email);
+  },
+};
